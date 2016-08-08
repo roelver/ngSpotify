d79e16b00f98ca776748251e1821b26b32a91672
roelver <roel@romaniflo.nl>
2016-08-08 20:18:24 +0200

    Setup page sketeton and routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
+	 moduleId: module.id,
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app.component.html',
+    directives: [NavbarComponent, ROUTER_DIRECTIVES],
+    precompile: [AboutComponent, SearchComponent]
 })
+
 export class AppComponent { }
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
-
+import { appRouterProviders } from './app.router';
 import { AppComponent } from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [appRouterProviders]);
